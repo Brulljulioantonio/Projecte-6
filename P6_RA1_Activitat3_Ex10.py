9cc706e0e87eb344e2893e8686bbb494ff403139
Brulljulioantonio <Brull@iesjulioantonio.cat>
2025-09-17 18:09:29 +0200

    .

--- P6_RA1_Activitat3_Ex10.py
+++ P6_RA1_Activitat3_Ex10.py
@@ -1,5 +1,10 @@
-número = int(input("número: "))
-if número % 2 == 0:
-    print("parell")
+edat1 = int(input("edat 1: "))
+edat2 = int(input("edat 2: "))
+if edat1 > edat2:
+    print("l'edat 1 es major")
+elif edat2 > edat1:
+    print("l'edat 2 es major")
+elif edat1 == edat2:
+    print("són iguals")
 else:
-    print("senar")
+    print("error")
