4203a25d02c4360216bfea4643c4fd5d57ba079c
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-10-01 10:39:03 +0200

    .

---
+++ 2.1 RA2_ControlFluxI/2.1 RA2_ControlFluxl 1.py
@@ -0,0 +1,14 @@
+# Què fa el programa: comprova si es major, menor o igual que zero
+# Autor: Biel Rull Simon
+# Data: 1/10/25
+
+# Versió 1.0
+
+num = int(input("introdueix un numero "))
+
+if num > 0:
+    print("major a 0")
+if num < 0:
+    print("menor que 0")
+if num == 0:
+    print("igual a 0")
