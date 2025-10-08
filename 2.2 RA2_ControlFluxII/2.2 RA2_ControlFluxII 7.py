a28bc712ffce96887acd0c42ea30195900288e1c
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-10-08 10:32:05 +0200

    .

--- 2.2 RA2_ControlFluxII/2.2 RA2_ControlFluxII 7.py
+++ 2.2 RA2_ControlFluxII/2.2 RA2_ControlFluxII 7.py
@@ -4,3 +4,12 @@
 
 # Versió 1.0
 
+texto = input("Text a invertir: ")
+invertido = ""
+i = len(texto) - 1
+
+while i >= 0:
+    invertido += texto[i]
+    i -= 1
+
+print("Texto invertit:", invertido)
