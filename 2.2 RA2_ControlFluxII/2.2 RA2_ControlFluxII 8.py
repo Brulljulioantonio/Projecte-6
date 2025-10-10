90bdf6fcd48c64afc5282a2ab626c5ef3287fe59
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-10-10 10:46:32 +0200

    .

--- 2.2 RA2_ControlFluxII/2.2 RA2_ControlFluxII 8.py
+++ 2.2 RA2_ControlFluxII/2.2 RA2_ControlFluxII 8.py
@@ -4,3 +4,14 @@
 
 # Versió 1.0
 
+paraula = input("Introdueix una paraula: ")
+
+vocals = "aeiouAEIOU"
+
+contador = 0
+
+for i in paraula:
+    if i in vocals:
+        contador += 1
+
+print("La paraula conté" , contador , "vocals")
