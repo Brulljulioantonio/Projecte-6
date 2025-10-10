5783b50750b3280ec34bf39ece0aa03b5c1d6d05
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-10-10 10:50:56 +0200

    .

--- 2.2 RA2_ControlFluxII/2.2 RA2_ControlFluxII 9.py
+++ 2.2 RA2_ControlFluxII/2.2 RA2_ControlFluxII 9.py
@@ -4,3 +4,10 @@
 
 # Versió 1.0
 
+nums = input("Introdueix numeros separats per espais: ")
+nums = nums.split()
+maxim = float(nums[0])
+for i in nums:
+    if float(i) > maxim:
+        maxim = float(i)
+print("El numero mes gran es:", maxim)
