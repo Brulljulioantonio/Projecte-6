e93e45cdbfb4eaaa11b32f2390614af68ead97da
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-10-10 09:23:53 +0000

    .

--- 2.3 RA2_ControlFluxIII/2.3 RA2_ControlFluxIII 01.py
+++ 2.3 RA2_ControlFluxIII/2.3 RA2_ControlFluxIII 01.py
@@ -1,8 +1,12 @@
-# Què fa el programa:
+# Què fa el programa: Aquest programa demana a l'usuari un número i realitza un compte enrere des d'aquest número fins a zero.
 # Autor: Biel Rull Simon
-# Data: 
+# Data: 10/10/2025
 
-# Versió
+# Versió 1.0
 
 # Especificacions d'entrada
-
+num = -1
+while num <= 0:
+    num = int(input("Introdueix un número per fer el compte enrere: "))
+for i in range(num, -1, -1):
+    print(i)
