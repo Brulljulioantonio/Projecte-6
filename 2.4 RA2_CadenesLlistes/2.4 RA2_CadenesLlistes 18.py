5f1b10859a02655c58a053407d599d1a1102b4f7
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-10-31 11:59:44 +0000

    .

---
+++ 2.4 RA2_CadenesLlistes/2.4 RA2_CadenesLlistes 18.py
@@ -0,0 +1,9 @@
+# Què fa el programa: Escriu una funció que retorni la llista al revés (sense reverse()).
+# Autor: Biel Rull Simon
+# Data: 22/10/2025
+
+# Versió 1.0
+
+llista = input("Introdueix elements separats per espais: ").split()
+llista = llista[::-1]
+print(llista)
