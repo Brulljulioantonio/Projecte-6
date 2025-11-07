816607ba258957b37b6fb0aebdd636fc7c0f2bca
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-11-07 12:06:33 +0000

    .

--- 2.5 RA2_FuncionsModuls/2.5 RA2_FuncionsModuls 05.py
+++ 2.5 RA2_FuncionsModuls/2.5 RA2_FuncionsModuls 05.py
@@ -1,11 +1,11 @@
 # Què fa el programa: Escriu una funció saluda_nom(nom="amic") que imprimeixi "Hola, <nom>". Si no passes cap nom, ha de imprimir "Hola, amic".
 # Autor: Biel Rull Simon
 
-nom = input("Introdueix el teu nom: ")
+nom = "amic"
 
-def saluda_nom(nom="amic"):
-    if nom == "":
-        nom = "amic"
+def saluda_nom(nom):
     print("Hola", nom)
     return
+saluda_nom("Joan")
 saluda_nom(nom)
+saluda_nom("Laia")
