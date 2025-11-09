dad271dc52749c79582d3ced3d1cd5b213fe44d1
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-11-09 18:15:57 +0100

    .

--- 2.5 RA2_FuncionsModuls/Mòduls/2.5 RA2_02/2.5 RA2_FuncionsModuls 02.py
+++ 2.5 RA2_FuncionsModuls/Mòduls/2.5 RA2_02/2.5 RA2_FuncionsModuls 02.py
@@ -1,3 +1,9 @@
 # Què fa el programa:
 # Autor: Biel Rull Simon
 
+import Conversions
+
+temperatures = float(input("Introdueix una temperatura en graus Celsius o Fahrenheit: "))
+
+print(Conversions.celsius_a_fahrenheit(temperatures), "Fº")
+print(Conversions.fahrenheit_a_celsius(temperatures), "Cº")
--- 2.5 RA2_FuncionsModuls/Mòduls/2.5 RA2_02/Conversions.py
+++ 2.5 RA2_FuncionsModuls/Mòduls/2.5 RA2_02/Conversions.py
@@ -1,8 +1,8 @@
 # Què fa el programa:
 # Autor: Biel Rull Simon
 
-def celsius_a_fahrenheit(c):
-    return (c * 9/5) + 32
+def celsius_a_fahrenheit(temperatura):
+    return (temperatura * 9/5) + 32
 
-def fahrenheit_a_celsius(f):
-    return (f - 32) * 5/9
+def fahrenheit_a_celsius(temperatura):
+    return (temperatura - 32) * 5/9
