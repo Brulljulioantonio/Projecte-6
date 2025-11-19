3ff7b0658d4e1a8953397acffc00fc92b955f548
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-11-19 08:29:16 +0000

    .

--- 2.6 RA2_Fitxers/2.6 RA2_Fitxers 08/2.6 RA2_Fitxers 08.py
+++ 2.6 RA2_Fitxers/2.6 RA2_Fitxers 08/2.6 RA2_Fitxers 08.py
@@ -1,14 +1,11 @@
 # Què fa el programa: Suposa que tens un fitxer nombres.txt que hauria de contenir un nombre enter per línia. Fes un programa que llegeixi cada línia i la transformi en enter. Si alguna línia no és un enter vàlid, captura l’error i mostra un missatge, però continua amb la resta.
 # Autor: Biel Rull Simon
 
-try:
-    fitxer = open("/workspaces/Projecte-6/2.6 RA2_Fitxers/2.6 RA2_Fitxers 08/nombres.txt", "r")
-    for linia in fitxer:
-        try:
-            nombre = int(linia.strip())
-            print(f"Nombre llegit: {nombre}")
-        except ValueError:
-            print(f"Error: La línia '{linia.strip()}' no és un enter vàlid.")
-    fitxer.close()
-except FileNotFoundError:
-    print("Error: El fitxer nombres.txt no existeix.")
+fitxer = open("/workspaces/Projecte-6/2.6 RA2_Fitxers/2.6 RA2_Fitxers 08/nombres.txt", "r")
+for i in fitxer:
+    try:
+        nombre = int(i)
+        print("Nombre llegit: ", nombre)
+    except ValueError:
+        print("Error: La línia ",i, " no és un enter vàlid.")
+fitxer.close()
