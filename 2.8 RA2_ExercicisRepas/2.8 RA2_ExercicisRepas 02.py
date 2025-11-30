3a381d493cbbe8453781a1235ba467a66dae3242
Brulljulioantonio <brull@iesjulioantonio.cat>
2025-11-30 15:17:22 +0100

    .

--- 2.8 RA2_ExercicisRepas/2.8 RA2_ExercicisRepas 02.py
+++ 2.8 RA2_ExercicisRepas/2.8 RA2_ExercicisRepas 02.py
@@ -1,7 +1,21 @@
 # Què fa el programa:
 # Autor: Biel Rull Simon
 
-num1 = int(input("Introdueix el valor 1: "))
-num2 = int(input("Introdueix el valor 2: "))
-num3 = int(input("Introdueix el valor 3: "))
+try:
 
+    num1 = int(input("Introdueix el valor 1: "))
+    num2 = int(input("Introdueix el valor 2: "))
+    num3 = int(input("Introdueix el valor 3: "))
+
+
+    if num1 > num2 and num1 > num3:
+        print("El valor 1 (",num1,") És mes gran")
+    elif num2 > num1 and num2 > num3:
+        print("El valor 2 (",num2,") És mes gran")
+    elif num3 > num1 and num3 > num2:
+        print("El valor 3 (",num3,") És mes gran")
+    elif num1 == num2 and num1 == num3:
+        print("Són iguals")
+
+except ValueError:
+    print("Valor no valid")
