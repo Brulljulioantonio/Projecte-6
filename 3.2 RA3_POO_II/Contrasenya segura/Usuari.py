aabb0a7a402ad834ec1ef69ae1ef8ad8c92f0a65
Brulljulioantonio <brull@iesjulioantonio.cat>
2026-01-23 11:52:05 +0000

    .

--- 3.2 RA3_POO_II/Contrasenya segura/Usuari.py
+++ 3.2 RA3_POO_II/Contrasenya segura/Usuari.py
@@ -1,20 +1,29 @@
+# Autor: Biel Rull Simon
+# Descripció: Aquesta classe Usuari permet gestionar la contrasenya d'un usuari.
+# La contrasenya es guarda en un atribut privat (__contrasenya) i es pot
+# llegir amb un getter i canviar amb un setter. El setter comprova que la
+# contrasenya sigui vàlida abans de modificar-la.
+
 class Usuari:
     def __init__(self, contrasenya):
+        # Constructor: inicialitza l'atribut privat __contrasenya amb la contrasenya donada
         self.__contrasenya = contrasenya
 
     @property
     def contrasenya(self):
+        # Getter: permet llegir la contrasenya de l'usuari
         return self.__contrasenya
 
     @contrasenya.setter
     def contrasenya(self, clau):
+        # Setter: permet canviar la contrasenya només si passa totes les validacions
+        # Comprova longitud mínima, almenys una majúscula i almenys un número
         if self.len_minim(clau) and self.w_upper(clau) and self.w_num(clau):
             self.__contrasenya = clau
             print("Contrasenya canviada correctament")
         else:
             print("Error: contrasenya no vàlida")
 
-    # Validacions
     def len_minim(self, clau):
         return len(clau) >= 8
 
