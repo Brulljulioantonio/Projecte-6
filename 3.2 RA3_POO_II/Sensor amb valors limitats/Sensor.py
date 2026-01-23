3e860d4f34747547d1d1fc6ccaabc846d2477a39
Brulljulioantonio <brull@iesjulioantonio.cat>
2026-01-23 09:55:40 +0000

    .

--- 3.2 RA3_POO_II/Sensor amb valors limitats/Sensor.py
+++ 3.2 RA3_POO_II/Sensor amb valors limitats/Sensor.py
@@ -1,15 +1,26 @@
+# Autor: Biel Rull Simon
+# Descripció: Aquesta classe Sensor gestiona un valor del sensor entre 0 i 100.
+# Permet:
+# 1. Llegir el valor amb un getter
+# 2. Modificar el valor amb un setter amb control de rang
+
 class Sensor:
     def __init__(self, valor):
+        # Constructor de la classe
+        # Inicialitza l'atribut privat __valor amb el valor inicial
         self.__valor = valor
         
     @property
     def v_sensor(self):
+        # Getter: permet llegir el valor del sensor
+        # No modifica el valor, només el retorna
         return self.__valor
     
     @v_sensor.setter
     def v_sensor(self, n_valor):
+        # Setter: permet canviar el valor del sensor només si està entre 0 i 100
         if 0 <= n_valor <= 100:
-
             self.__valor = n_valor
         else:
-            print("El valor ha de estar entre 0 i 100")
+            # Si el valor no és vàlid, mostra un missatge d'error
+            print("Error: el valor ha d'estar entre 0 i 100")
